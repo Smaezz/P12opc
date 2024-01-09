85cdff50279c059e2abd3a3000684a33ca314e8b
Smaezz <lea.msi@hotmail.fr>
2024-01-09 17:10:54 +0100

    mail2Netlify

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -30,7 +30,7 @@ export const ContactUs = () => {
 
     emailjs
       .send(
-        contactConfig.service_1hotbrn,
+        contactConfig.YOUR_SERVICE_ID,
         contactConfig.YOUR_TEMPLATE_ID,
         templateParams,
         contactConfig.YOUR_USER_ID
